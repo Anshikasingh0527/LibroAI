304f43e5c102a569d1d8377f97f1b6d2f868f53a
Anshika Singh <singhanshika15665@gmain.com>
2025-06-30 14:06:04 +0530

    Initial commit - LibroAI

---
+++ app/src/main/java/com/example/libroai/BookAdapter.java
@@ -0,0 +1,67 @@
+package com.example.libroai;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder> {
+
+    Context context;
+    ArrayList<BookModel> bookList;
+
+    public BookAdapter(Context context, ArrayList<BookModel> bookList) {
+        this.context = context;
+        this.bookList = bookList;
+    }
+
+    @NonNull
+    @Override
+    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.book_item, parent, false);
+        return new BookViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
+        BookModel book = bookList.get(position);
+
+        holder.bookTitle.setText(book.getTitle());
+        holder.bookAuthor.setText("by " + book.getAuthor());
+        holder.bookDesc.setText(book.getDescription());
+
+
+        // Load book image using Glide
+        Glide.with(context)
+                .load(book.getImageUrl())
+                .placeholder(R.drawable.img_2) // default image
+                .into(holder.bookImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookList.size();
+    }
+
+    public class BookViewHolder extends RecyclerView.ViewHolder {
+        TextView bookTitle, bookAuthor, bookDesc;
+        ImageView bookImage;
+
+        public BookViewHolder(@NonNull View itemView) {
+            super(itemView);
+            bookTitle = itemView.findViewById(R.id.bookTitle);
+            bookAuthor = itemView.findViewById(R.id.bookAuthor);
+            bookDesc = itemView.findViewById(R.id.bookDesc);
+            bookImage = itemView.findViewById(R.id.bookImage);
+        }
+    }
+}
